55c474c71325706d68a4c9114abbf8b51687220b
Elwick1 <michaelrwilliamson92@gmail.com>
2021-01-19 17:54:20 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+// import PokemonContainer from './'
 
-function App() {
+
+export default class App extends React.Component {
+
+  state = {
+    pokemon : [],
+    showPokemon : false
+  }
+
+  componentDidMount() {
+    for (let i = 1; i < 152; i++) {
+      const API = `https://pokeapi.co/api/v2/pokemon/${i}`
+      fetch(API)
+      .then(res => res.json())
+      .then(pkmn => this.setState({
+        pokemon : pkmn
+      }))
+    }
+  }
+
+  handleClick = () => {
+    this.setState({
+      showPokemon : true
+    })
+  }
+
+
+
+  render() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to the world of pokemon!</h1>
+        {/* {console.log(this.state.pokemon)} */}
+        <button onClick={this.handleClick}> Let's Start </button>
+        {/* {this.state.showPokemon === true ? <PokemonContainer/> : null} */}
       </header>
     </div>
   );
 }
+};
 
-export default App;
+
